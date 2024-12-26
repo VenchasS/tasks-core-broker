61d6b9f68ce5a53abf7c5c589dd12fb60d402250
VenchasS <49115035+VenchasS@users.noreply.github.com>
2024-12-26 13:34:31 +0500

    Delete tasks-core-broker/Tasks directory

--- tasks-core-broker/Tasks/Models/Models.cs
+++
@@ -1,9 +0,0 @@
-namespace TaskExecutor.Models
-{
-    public class Models
-    {
-        public string Id { get; set; }          // Идентификатор задачи
-        public string Status { get; set; }      // Статус выполнения (успех, ошибка, истек TTL)
-        public string Result { get; set; }      // Результат выполнения
-    }
-}
